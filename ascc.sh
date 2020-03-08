309feaaf85e99bb4e6bedb86cc418784ca822cd2
Sören Ritter <soeren@srage.de>
2020-03-08 17:32:00 +0100

    Update ascc.sh

--- ascc.sh
+++ ascc.sh
@@ -2,30 +2,30 @@
 
 /usr/bin/screen -d -m /dev/ttyUSB0 9600
 
-fanspeed_state=`cat /tmp/servermon.state | cut -d "," -f1`
-r_state=`cat /tmp/servermon.state | cut -d "," -f2`
-g_state=`cat /tmp/servermon.state | cut -d "," -f3`
-b_state=`cat /tmp/servermon.state | cut -d "," -f4`
+fanspeed_state=`cat /tmp/ascc.state | cut -d "," -f1`
+r_state=`cat /tmp/ascc.state | cut -d "," -f2`
+g_state=`cat /tmp/ascc.state | cut -d "," -f3`
+b_state=`cat /tmp/ascc.state | cut -d "," -f4`
 
 case "$1" in
 -f)
     echo -e $2,$r_state,$g_state,$b_state > /tmp/servermon.state
     ;;
 -c)
-    echo -e $fanspeed_state,$2 > /tmp/servermon.state
+    echo -e $fanspeed_state,$2 > /tmp/ascc.state
     ;;
 -r)
-    echo -e $fanspeed_state,$2,$g_state,$b_state > /tmp/servermon.state
+    echo -e $fanspeed_state,$2,$g_state,$b_state > /tmp/ascc.state
     ;;
 -g)
-    echo -e $fanspeed_state,$r_state,$2,$b_state > /tmp/servermon.state
+    echo -e $fanspeed_state,$r_state,$2,$b_state > /tmp/ascc.state
     ;;
 -b)
-    echo -e $fanspeed_state,$r_state,$g_state,$2 > /tmp/servermon.state
+    echo -e $fanspeed_state,$r_state,$g_state,$2 > /tmp/ascc.state
     ;;
 *)
-    echo -e $1 > /tmp/servermon.state
+    echo -e $1 > /tmp/ascc.state
     ;;
 esac
 
-/bin/cat /tmp/servermon.state > /dev/ttyUSB0
+/bin/cat /tmp/ascc.state > /dev/ttyUSB0
